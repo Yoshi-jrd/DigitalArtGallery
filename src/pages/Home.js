31881464dd03d3d8a880299e79c294ea331083f4
Josh DeVore <jrd1185@gmail.com>
2024-09-22 15:18:48 -0600

    completed hero banner.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,19 +5,28 @@ function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="hero-banner" style={{
-        backgroundImage: `url(/images/ArtHeroImage3.jpg)`, // Adjusted path
-        height: '100vh', // Full viewport height
-        backgroundSize: 'cover', // Cover the entire div
-        backgroundPosition: 'center', // Center the background image
+        backgroundImage: `url(/images/ArtHeroImage2.jpg)`,
+        height: '100vh',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        color: 'white' // Text color for visibility
+        color: 'white',
+        textShadow: '2px 2px 10px rgba(0,0,0,0.9)'  // Enhanced text shadow for better readability
       }}>
-        <div>
-          <h1 className="text-white text-5xl font-light mb-6">Discover Unique Artworks</h1>
-          <p className="text-gray-300 text-lg font-light mb-4">
+        <div style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',  // More opaque backdrop for better text visibility
+          padding: '20px',
+          borderRadius: '10px',
+          width: '80%',  // Ensures text block does not stretch too wide
+          textAlign: 'center'  // Centers text inside the block
+        }}>
+          <h1 className="text-white text-5xl font-bold mb-6">Discover Unique Artworks</h1>
+          <p className="text-white text-lg font-light mb-4" style={{ 
+            textShadow: '2px 2px 8px rgba(0,0,0,1)'  // Strong shadow for subtitle
+          }}>
             Explore the world through art from the comfort of your home.
           </p>
           <Link to="/gallery" className="bg-transparent border-2 border-white text-white py-2 px-6 hover:bg-white hover:text-gray-900 transition duration-300">
